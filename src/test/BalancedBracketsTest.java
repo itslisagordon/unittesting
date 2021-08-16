c53dafcf396682dc8f95893620a2bf3822fab8bd
Lisa Gordon <lg.official94@gmail.com>
2021-08-15 19:12:31 -0500

    finished studio re-do

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,6 +12,24 @@ public class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    //add a test that makes sure brackets are balanced
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    //add a test to make sure brackets don't go negative
+    @Test
+    public void unbalancedReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]"));
+    }
+
+    @Test
+    public void negativeReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]]]"));
+    }
+
 
 
 }
